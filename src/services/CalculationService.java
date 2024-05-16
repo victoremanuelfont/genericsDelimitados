9e2ce8274b4a74a412a8ee6f1423af6c7be6fca2
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-16 11:20:25 -0300

    Exemplo comparando valores com tipo variados

--- src/services/CalculationService.java
+++ src/services/CalculationService.java
@@ -3,13 +3,18 @@ package services;
 import java.util.List;
 
 public class CalculationService {
+	/*
+	 * Adaptação do max para que funcione com qualquer tipo. Para que não de erro no
+	 * .compareTo, pois ele não está definido pelo tipo T, é necessário especificar
+	 * que o tipo T é do tipo comparable <T extends Comparable<T>.
+	 */
 
-	public static Integer max(List<Integer> list) {
+	public static <T extends Comparable<T>> T max(List<T> list) {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List can't be empty");
 		}
-		Integer max = list.get(0);
-		for (Integer item : list) {
+		T max = list.get(0);
+		for (T item : list) {
 			if (item.compareTo(max) > 0) {
 				max = item;
 			}
